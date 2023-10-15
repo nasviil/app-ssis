7db1b3703f42daf2321e19c8ec0c8159bbbfe85a
nasviil <mrnasviil@gmail.com>
2023-10-15 22:43:55 +0800

    updated python files

--- website/__init__.py
+++ website/__init__.py
@@ -2,12 +2,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET KEY']='key'
+    app.config['SECRET KEY']='super secret key'
 
     from .views import views
-    from .auth import auth
 
     app.register_blueprint(views,url_prefix='/')
-    app.register_blueprint(auth,url_prefix='/')
 
     return app
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 
 views = Blueprint('views', __name__)
 
@@ -9,6 +9,27 @@ def student():
 @views.route('/course')
 def course():
     return render_template("course.html")
+
+@views.route('/college')
+def college():
+    return render_template("college.html")
+
+@views.route('/student/add', methods=['GET', 'POST'])
+def add_student():
+    if request.method =='POST':
+        data = request.form
+        print(data)
+        flash('Student added.', category='success')
+
+    return render_template("student-add.html")
+
+@views.route('/course/add', methods=['GET', 'POST'])
+def add_course():
+    return render_template("course-add.html")
+
+@views.route('/college/add', methods=['GET', 'POST'])
+def add_college():
+    return render_template("college-add.html")
 
 @views.route('/')
 def index():
