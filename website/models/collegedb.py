34abf40064e7492f79c63c7d9f08b86d584ab086
nasviil <mrnasviil@gmail.com>
2023-10-18 22:57:48 +0800

    added add function

---
+++ website/models/collegedb.py
@@ -0,0 +1,23 @@
+from website import mysql
+
+class College:
+    __tablename__ = 'college'
+
+    def __init__(self, name=None, code=None, id=None):
+        self.id = id
+        self.name = name
+        self.code = code
+
+    def insert(self):
+        INSERT_SQL = f"INSERT INTO {self.__tablename__} (id, name, code) VALUES (%s, %s, %s)"
+        cur = mysql.connection.cursor()
+        cur.execute(INSERT_SQL, (self.id, self.name, self.code))
+        mysql.connection.commit()
+        
+    @classmethod
+    def get_colleges(cls):
+        SELECT_SQL = f"SELECT * FROM {cls.__tablename__}"
+        cur = mysql.new_cursor(dictionary=True)
+        cur.execute(SELECT_SQL)
+        colleges = cur.fetchall()
+        return colleges
